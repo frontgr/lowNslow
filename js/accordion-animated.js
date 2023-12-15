3512f4106c0c98343a50bedfe81129480a4fceb9
oleg puck <grinvaurdourin@gmail.com>
2023-12-15 04:05:00 +0300

    add html: section services + add js: accordion

---
+++ js/accordion-animated.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const accordions = document.querySelectorAll(".accordion__item");
+
+  accordions.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      const self = e.currentTarget;
+      const control = self.querySelector(".accordion__control");
+      const content = self.querySelector(".accordion__content");
+      const desc = self.querySelector(".accordion__desc");
+
+      self.classList.toggle("open");
+
+      //  Если открыт аккордион
+      if (self.classList.contains("open")) {
+        control.setAttribute("aria-expanded", "true");
+        control.style.marginBottom = "10px";
+        control.style.transition = "margin 0.3s ease";
+        content.setAttribute("aria-hidden", "false");
+        desc.style.opacity = 1;
+        content.style.transition = "all 1s ease";
+        content.style.maxHeight = content.scrollHeight + "px";
+      } else {
+        control.setAttribute("aria-expanded", "false");
+        control.style.marginBottom = "0";
+        desc.style.opacity = 0;
+        content.setAttribute("aria-hidden", "true");
+        content.style.maxHeight = 0;
+      }
+    });
+  });
+});
