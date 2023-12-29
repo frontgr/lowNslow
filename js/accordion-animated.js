8042d939e00d797910a2520a5551510f9ea6b83d
WhyDojo <grinvaurdourin@gmail.com>
2023-12-29 20:03:13 +0300

    change accordion

--- js/accordion-animated.js
+++ js/accordion-animated.js
@@ -15,17 +15,21 @@ document.addEventListener("DOMContentLoaded", () => {
         control.setAttribute("aria-expanded", "true");
         control.style.transition = "margin 0.3s ease";
         content.setAttribute("aria-hidden", "false");
-        control.style.borderRadius = "0";
-        content.style.borderRadius = "0";
         desc.style.opacity = 1;
+        control.style.borderRadius = "10px 10px 0 0 ";
+        content.style.borderRadius = "0 0 10px 10px";
         content.style.transition = "all 1s ease";
+        control.style.transition = "margin 1s ease";
         content.style.border = "1px solid #e2e2e2";
+        control.style.borderBottom = "none";
         content.style.borderTop = "0";
         content.style.maxHeight = content.scrollHeight + "px";
       } else {
         control.setAttribute("aria-expanded", "false");
         control.style.marginBottom = "0";
-        control.style.borderRadius = "10";
+        control.style.borderRadius = "10px";
+        control.style.transition = "margin 1s ease";
+        control.style.borderBottom = "1px solid #e2e2e2";
         desc.style.opacity = 0;
         content.setAttribute("aria-hidden", "true");
         content.style.maxHeight = 0;
